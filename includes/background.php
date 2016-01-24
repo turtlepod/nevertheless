ba6eade889c464d1aa825c82b70de3b0a94ee537
David Chandra <david@shellcreeper.com>
2016-01-24 10:01:39 +0700

    custom background.

--- includes/background.php
+++ includes/background.php
@@ -5,8 +5,10 @@
 
 /* === Custom Background === */
 $custom_backgound_args = array(
-	'default-color'          => '',
-	'default-image'          => '',
+	'default-color'          => '#c9651c',
+	'default-image'          => get_template_directory_uri() . '/assets/images/bg.png',
+	'default-repeat'         => 'repeat-x',
+	'default-position-x'     => 'left',
 	'wp-head-callback'       => '_custom_background_cb',
 );
 add_theme_support( 'custom-background', $custom_backgound_args );
