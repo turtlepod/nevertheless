74f9b2665c943ac543d18be7e0b1c287ccf39c92
David <david@shellcreeper.com>
2017-11-17 10:43:49 +0700

    BUtton Editor Style #18

--- includes/custom-colors.php
+++ includes/custom-colors.php
@@ -290,6 +290,9 @@ function nevertheless_color_mce_css_ajax_callback(){
 
 	/* Set File Type */
 	header( 'Content-type: text/css' );
-	echo wp_strip_all_tags( "a,a:hover,a:focus{color:#{$color_link}}" );
+	$css  = '';
+	$css .= "a,a:hover,a:focus{color:#{$color_link}}";
+	$css .= 'input[type="submit"]:hover,input[type="submit"]:focus,input[type="button"]:hover,input[type="button"]:focus,input[type="reset"]:hover,input[type="reset"]:focus,button:hover,button:focus,.button:hover,.button:focus{ border-color: #' . $color_link . '; background: #' . $color_link . '; color: #fff; }';
+	echo wp_strip_all_tags( $css );
 	die();
 }
